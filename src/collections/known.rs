7a64044513bacb1f0ef8790d59c79912b703463b
Equal Ma <equalma@outlook.com>
2024-08-10 16:17:04 +0800

    wip: rework known collection

--- src/collections/known.rs
+++ src/collections/known.rs
@@ -1,45 +1,214 @@
-use crate::define_known_variants;
+use crate::const_known::{No, Yes, YesOrNo};
 
-use super::{array_vec::ArrayVec, count::Count, lead_vec::LeadVec};
+use super::{
+    array_vec::ArrayVec, collect_nothing::CollectNothing, count::Count, lead_vec::LeadVec,
+    HasConstDummyValue,
+};
 
-define_known_variants!(
-    #[non_exhaustive]
-    #[derive(Clone, Copy)]
-    pub enum KnownCollection<T, const CAP: usize> {
-        /// Panics if pushing values when filled.
-        ArrayVec(ArrayVec<T, CAP>),
-        /// Only keep the leading values and records the real count.
-        LeadVec(LeadVec<T, CAP>),
-        /// Just count.
-        Count(Count),
-    }
+#[non_exhaustive]
+#[derive(Clone, Copy)]
+pub enum KnownCollection<V: IsKnownCollection> {
+    ArrayVec(V::ArrayVec, V::ArrayVecType),
+    LeadVec(V::LeadVec, V::LeadVecType),
+    Count(V::Count, Count),
+    CollectNothing(V::CollectNothing, CollectNothing),
+}
 
-    #[sealed(sealed)]
+mod sealed {
     pub trait IsKnownCollection {}
+    pub trait IsArrayVec {}
+    pub trait IsLeadVec {}
+}
 
-    #[from_variant]
-    pub fn from_collection();
+pub trait IsArrayVec: sealed::IsArrayVec {
+    type Item;
+}
+impl<T, const N: usize> sealed::IsArrayVec for ArrayVec<T, N> {}
+impl<T, const N: usize> IsArrayVec for ArrayVec<T, N> {
+    type Item = T;
+}
 
-    // #[into_variant]
-    // fn into_collection();
+pub trait IsLeadVec: sealed::IsLeadVec {
+    type Item;
+}
+impl<T, const N: usize> sealed::IsLeadVec for LeadVec<T, N> {}
+impl<T, const N: usize> IsLeadVec for LeadVec<T, N> {
+    type Item = T;
+}
 
-    #[as_variant]
-    pub fn as_collection();
-);
+pub trait IsKnownCollection: sealed::IsKnownCollection + Sized {
+    type ArrayVecType: IsArrayVec<Item = <Self::LeadVecType as IsLeadVec>::Item>;
+    type ArrayVec: YesOrNo;
+    type LeadVecType: IsLeadVec;
+    type LeadVec: YesOrNo;
+    type Count: YesOrNo;
+    type CollectNothing: YesOrNo;
+}
 
-impl<V: IsKnownCollection<T, CAP>, T: std::fmt::Debug, const CAP: usize> std::fmt::Debug
-    for KnownCollection<V, T, CAP>
+pub trait IsKnownCollectionWithConstEmpty: IsKnownCollection {
+    const EMPTY_COLLECTION: Self;
+    const EMPTY_COLLECTION_AS_KNOWN_COLLECTION: KnownCollection<Self>;
+}
+
+impl<T, const CAP: usize> sealed::IsKnownCollection for ArrayVec<T, CAP> {}
+impl<T, const CAP: usize> IsKnownCollection for ArrayVec<T, CAP> {
+    type ArrayVecType = Self;
+    type ArrayVec = Yes;
+    type LeadVecType = LeadVec<T, 0>;
+    type LeadVec = No;
+    type Count = No;
+    type CollectNothing = No;
+}
+impl<T: HasConstDummyValue, const CAP: usize> IsKnownCollectionWithConstEmpty for ArrayVec<T, CAP> {
+    const EMPTY_COLLECTION: Self = Self::EMPTY;
+    const EMPTY_COLLECTION_AS_KNOWN_COLLECTION: KnownCollection<Self> =
+        KnownCollection::<Self>::from_variant(Self::EMPTY);
+}
+
+impl<T, const CAP: usize> sealed::IsKnownCollection for LeadVec<T, CAP> {}
+impl<T, const CAP: usize> IsKnownCollection for LeadVec<T, CAP> {
+    type ArrayVecType = ArrayVec<T, 0>;
+    type ArrayVec = No;
+    type LeadVecType = Self;
+    type LeadVec = Yes;
+    type Count = No;
+    type CollectNothing = No;
+}
+impl<T: HasConstDummyValue, const CAP: usize> IsKnownCollectionWithConstEmpty for LeadVec<T, CAP> {
+    const EMPTY_COLLECTION: Self = Self::EMPTY;
+    const EMPTY_COLLECTION_AS_KNOWN_COLLECTION: KnownCollection<Self> =
+        KnownCollection::<Self>::from_variant(Self::EMPTY);
+}
+
+impl sealed::IsKnownCollection for Count {}
+impl IsKnownCollection for Count {
+    type ArrayVecType = ArrayVec<No, 0>;
+    type ArrayVec = No;
+    type LeadVecType = LeadVec<No, 0>;
+    type LeadVec = No;
+    type Count = Yes;
+    type CollectNothing = No;
+}
+impl IsKnownCollectionWithConstEmpty for Count {
+    const EMPTY_COLLECTION: Self = Self::EMPTY;
+    const EMPTY_COLLECTION_AS_KNOWN_COLLECTION: KnownCollection<Self> =
+        KnownCollection::<Self>::from_variant(Self::EMPTY);
+}
+
+impl sealed::IsKnownCollection for CollectNothing {}
+impl IsKnownCollection for CollectNothing {
+    type ArrayVecType = ArrayVec<No, 0>;
+    type ArrayVec = No;
+    type LeadVecType = LeadVec<No, 0>;
+    type LeadVec = No;
+    type Count = No;
+    type CollectNothing = Yes;
+}
+impl IsKnownCollectionWithConstEmpty for CollectNothing {
+    const EMPTY_COLLECTION: Self = Self;
+    const EMPTY_COLLECTION_AS_KNOWN_COLLECTION: KnownCollection<Self> =
+        KnownCollection::<Self>::from_variant(Self);
+}
+
+impl<T, const CAP: usize> KnownCollection<ArrayVec<T, CAP>> {
+    pub const fn from_variant(v: ArrayVec<T, CAP>) -> Self {
+        Self::ArrayVec(Yes, v)
+    }
+}
+impl<T, const CAP: usize> KnownCollection<LeadVec<T, CAP>> {
+    pub const fn from_variant(v: LeadVec<T, CAP>) -> Self {
+        Self::LeadVec(Yes, v)
+    }
+}
+impl KnownCollection<Count> {
+    pub const fn from_variant(v: Count) -> Self {
+        Self::Count(Yes, v)
+    }
+}
+impl KnownCollection<CollectNothing> {
+    pub const fn from_variant(v: CollectNothing) -> Self {
+        Self::CollectNothing(Yes, v)
+    }
+}
+
+impl<T, const CAP: usize> KnownCollection<ArrayVec<T, CAP>> {
+    pub const fn as_variant(&self) -> &ArrayVec<T, CAP> {
+        match self {
+            Self::ArrayVec(Yes, this) => this,
+            Self::LeadVec(no, _) => match *no {},
+            Self::Count(no, _) => match *no {},
+            Self::CollectNothing(no, _) => match *no {},
+        }
+    }
+}
+impl<T, const CAP: usize> KnownCollection<LeadVec<T, CAP>> {
+    pub const fn as_variant(&self) -> &LeadVec<T, CAP> {
+        match self {
+            Self::ArrayVec(no, _) => match *no {},
+            Self::LeadVec(Yes, this) => this,
+            Self::Count(no, _) => match *no {},
+            Self::CollectNothing(no, _) => match *no {},
+        }
+    }
+}
+impl KnownCollection<Count> {
+    pub const fn as_variant(&self) -> &Count {
+        match self {
+            Self::ArrayVec(no, _) => match *no {},
+            Self::LeadVec(no, _) => match *no {},
+            Self::Count(Yes, this) => this,
+            Self::CollectNothing(no, _) => match *no {},
+        }
+    }
+}
+impl KnownCollection<CollectNothing> {
+    pub const fn as_variant(&self) -> &CollectNothing {
+        match self {
+            Self::ArrayVec(no, _) => match *no {},
+            Self::LeadVec(no, _) => match *no {},
+            Self::Count(no, _) => match *no {},
+            Self::CollectNothing(Yes, this) => this,
+        }
+    }
+}
+
+impl KnownCollection<CollectNothing> {
+    pub const fn into_variant(self) -> CollectNothing {
+        match self {
+            Self::ArrayVec(no, _) => match no {},
+            Self::LeadVec(no, _) => match no {},
+            Self::Count(no, _) => match no {},
+            Self::CollectNothing(Yes, this) => this,
+        }
+    }
+}
+
+impl<V, Item, const ARRAY_VEC_CAP: usize, const LEAD_VEC_CAP: usize> std::fmt::Debug
+    for KnownCollection<V>
+where
+    Item: std::fmt::Debug,
+    V: IsKnownCollection<
+        ArrayVecType = ArrayVec<Item, ARRAY_VEC_CAP>,
+        LeadVecType = LeadVec<Item, LEAD_VEC_CAP>,
+    >,
 {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Self::ArrayVec(_, this) => this.fmt(f),
             Self::LeadVec(_, this) => this.fmt(f),
             Self::Count(_, this) => this.fmt(f),
+            Self::CollectNothing(_, this) => this.fmt(f),
         }
     }
 }
 
-impl<V: IsKnownCollection<T, CAP>, T, const CAP: usize> KnownCollection<V, T, CAP> {
+impl<V, T, const ARRAY_VEC_CAP: usize, const LEAD_VEC_CAP: usize> KnownCollection<V>
+where
+    V: IsKnownCollection<
+        ArrayVecType = ArrayVec<T, ARRAY_VEC_CAP>,
+        LeadVecType = LeadVec<T, LEAD_VEC_CAP>,
+    >,
+{
     pub const fn with_push(self, value: T) -> Self
     where
         T: Copy,
@@ -53,6 +222,7 @@ impl<V: IsKnownCollection<T, CAP>, T, const CAP: usize> KnownCollection<V, T, CA
                 this.len += 1;
                 this
             }),
+            this @ KnownCollection::CollectNothing(_, _) => this,
         }
     }
 
@@ -75,6 +245,7 @@ impl<V: IsKnownCollection<T, CAP>, T, const CAP: usize> KnownCollection<V, T, CA
                 }),
                 Some(value),
             ),
+            this @ KnownCollection::CollectNothing(_, _) => (this, Some(value)),
         }
     }
 }
