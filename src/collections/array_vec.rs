bc0fcffb45e846b6260d4e249c7712884af98b33
Equal Ma <equalma@outlook.com>
2024-09-10 23:50:48 +0800

    feat: add ArrayVec::with_extend_from_slice, with_try_extend_from_slice, try_into_filled_array

--- src/collections/array_vec.rs
+++ src/collections/array_vec.rs
@@ -56,6 +56,33 @@ impl<T, const CAP: usize> ArrayVec<T, CAP> {
         }
     }
 
+    pub const fn with_extend_from_slice(self, s: &[T]) -> Self
+    where
+        T: Copy,
+    {
+        match self.with_try_extend_from_slice(s) {
+            Ok(this) => this,
+            Err(_) => panic!("filled"),
+        }
+    }
+
+    const fn with_try_extend_from_slice(mut self, mut s: &[T]) -> Result<Self, ([T; CAP], &[T])>
+    where
+        T: Copy,
+    {
+        while let Some((value, new_s)) = s.split_first() {
+            match self.with_try_push(*value) {
+                Ok(this) => {
+                    self = this;
+                    s = new_s;
+                }
+                Err((filled, _)) => return Err((filled, s)),
+            }
+        }
+
+        Ok(self)
+    }
+
     pub(crate) const fn first(&self) -> Option<&T> {
         if self.len > 0 {
             Some(&self.array[0])
@@ -135,5 +162,30 @@ impl<T: HasConstDummyValue, const CAP: usize> ArrayVec<T, CAP> {
             let (front, this) = self.with_pop_front();
             (front, this.with_push(value))
         }
+    }
+
+    /// Returns `Err(self)` if `M != self.len()`
+    ///
+    /// TODO: Without unsafe, this could be implemented without constraining `T: HasConstDummyValue`
+    /// with [`<[T]>::first_chunk`](https://doc.rust-lang.org/std/primitive.slice.html#method.first_chunk) but it requires rust 1.77
+    pub const fn try_into_filled_array<const M: usize>(self) -> Result<[T; M], Self>
+    where
+        T: Copy,
+    {
+        if M != self.len {
+            return Err(self);
+        }
+
+        Ok({
+            let mut res = [T::DUMMY_VALUE; M];
+
+            let mut i = 0;
+            while i < M {
+                res[i] = self.array[i];
+                i += 1;
+            }
+
+            res
+        })
     }
 }
